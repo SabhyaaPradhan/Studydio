71c05a8ca1e3712344b000fb7241ab02fc2f8348
Sabhyaa Pradhan <sabhyaapradhan2005@gmail.com>
2025-10-03 16:04:20 +0000

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function LandingPage() {
             brightness={0.4}
           />
         </div>
-        <section id="hero" className="w-full min-h-screen relative flex items-center justify-center bg-black/50">
+        <section id="hero" className="w-full min-h-screen relative flex items-center justify-center">
           <div className="container mx-auto px-4 md:px-6">
             <div className="grid gap-6 lg:grid-cols-1 lg:gap-12 xl:grid-cols-1">
               <div className="flex flex-col justify-center space-y-4 z-20 text-center items-center">
@@ -62,7 +62,7 @@ export default function LandingPage() {
           </div>
         </section>
 
-        <section id="how-it-works" className="w-full py-12 md:py-24 lg:pt-12 lg:py-32 bg-gradient-to-b from-stone-950 to-black">
+        <section id="how-it-works" className="w-full py-12 md:py-24 lg:pt-12 lg:py-32 bg-gradient-to-b from-transparent via-stone-950 to-black">
             <div className="container mx-auto px-4 md:px-6">
                 <div className="flex flex-col items-center justify-center space-y-4 text-center">
                     <div className="space-y-2">

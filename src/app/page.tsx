b46455243b5e4a3fde21852c3014fc804b284aae
Sabhyaa Pradhan <sabhyaapradhan2005@gmail.com>
2025-10-03 12:53:48 +0000

    Make the gradient bg not have opacity (_for element section_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,7 @@ export default function LandingPage() {
           </div>
         </section>
 
-        <section id="how-it-works" className="w-full py-12 md:py-24 lg:py-32 bg-gradient-to-b from-transparent to-background">
+        <section id="how-it-works" className="w-full py-12 md:py-24 lg:py-32 bg-gradient-to-b from-black to-background">
             <div className="container mx-auto px-4 md:px-6">
                 <div className="flex flex-col items-center justify-center space-y-4 text-center">
                     <div className="space-y-2">

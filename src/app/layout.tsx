a2b8bede24dc2a6479e1ec5a38d06f3f3e26fee7
Sabhyaa Pradhan <sabhyaapradhan2005@gmail.com>
2025-10-06 12:36:29 +0000

    Remove the gap that is below the footer (_for element footer_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="antialiased pb-24">
+      <body className="antialiased">
         <GlobalPixelTrail />
         {children}
         <Toaster />

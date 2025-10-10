1413f1f79c32d7ee4d29258998ec254017108852
Sabhyaa Pradhan <sabhyaapradhan2005@gmail.com>
2025-10-10 17:00:26 +0000

    22:29:23.193     at 29425 (.next/server/app/api/upload/route.js:1:30278

--- next.config.js
+++ next.config.js
@@ -29,6 +29,12 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('pdf-parse');
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;

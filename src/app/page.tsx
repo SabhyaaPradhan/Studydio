648ce6b880d104860e5e7a389df655537d87f454
Sabhyaa Pradhan <sabhyaapradhan2005@gmail.com>
2025-10-10 09:53:34 +0000

    make the bg transparent (_for element section_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -267,7 +267,7 @@ export default function LandingPage() {
           </div>
         </section>
 
-        <section id="stats" className="w-full py-12 md:py-24 lg:py-32 bg-black">
+        <section id="stats" className="w-full py-12 md:py-24 lg:py-32 bg-transparent">
           <div className="container mx-auto px-4 md:px-6">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
               <FadeContent>
@@ -403,6 +403,7 @@ export default function LandingPage() {
     </div>
   );
 }
+
 
 
 
